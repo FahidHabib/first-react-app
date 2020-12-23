7255d3504ea32bd54d7b8cd2b0a746783d4aeaf8
FahidHabib <fahid.habib84@gmail.com>
2020-12-23 13:01:18 +0500

    update app.js file

---
+++ src/Dinner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function Dinner(){
+    return(
+       <div>
+           <h1>Today i eat Chiken biryani</h1>
+           <h1>Today i eat Chiken Karahi</h1>
+
+        </div>
+    )
+}
+
+export default Dinner;
